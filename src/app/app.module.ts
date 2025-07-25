51b7679f54a9f46974db9ed2212f331ff3ec68a6
ITZEL CONCEPCION PALMA REYES <itzel.palma@totalplay.com.mx>
2025-07-24 21:08:47 -0600

    estructura web desavelopers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { CtaComponent } from './cta/cta.component';
+import { ResenasComponent } from './resenas/resenas.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ServiciosComponent,
+    BeneficiosComponent,
+    PortafolioComponent,
+    CtaComponent,
+    ResenasComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
